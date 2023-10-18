7277f3874fb123546b8f89568acb3ebf4485d8e0
mrSergi0 <88852075+mrSergi0@users.noreply.github.com>
2023-10-18 19:37:40 +0300

    task3 and task4 done

---
+++ Tasks/Lenivko/Task1/task4.py
@@ -0,0 +1,5 @@
+# This program converts USD to BYN.
+
+amount_of_usd = float(input('Please, enter your amount of USD ' ))
+byn_per_usd = 3.27
+print(f'{amount_of_usd} USD equals to {amount_of_usd * byn_per_usd} BYN')
