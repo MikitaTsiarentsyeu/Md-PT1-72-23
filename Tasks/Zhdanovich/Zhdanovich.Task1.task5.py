6ba0d247e5d6ccf107cf10717994c2eb63f9789f
DzmitryZhdanovich <dithelm@gmail.com>
2023-10-20 11:21:28 +0300

    5

--- Tasks/Zhdanovich/Zhdanovich.Task1.task5.py
+++
@@ -1,9 +0,0 @@
-import random
-try:
-    # enter the left border:
-    a=int(input("Enter the left border"))
-    # enter the right border:
-    b=int(input("Enter the left border"))
-    print (random.randint(a,b))
-except (ValueError, NameError, AttributeError):
-    print("You can only enter a numeric value")
