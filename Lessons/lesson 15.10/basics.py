142ec4aa198d58c15da023d309617421c11c8a0d
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2023-10-21 09:59:50 +0300

    add lesson 15.10

---
+++ Lessons/lesson 15.10/basics.py
@@ -0,0 +1,76 @@
+54313576846513846
+
+10.3543543
+
+"test string"
+
+x = 10
+
+print(x)
+
+x = 12.5
+
+print(x)
+
+y = 10*x
+print(y)
+
+x = y = 10
+
+print(x, y)
+
+x = 1
+
+print(2+2, 4*x, x+x+x+x)
+
+-10
+
+12-10
+
+x = 10
+x, y = 10, "new obj"
+print(x, y)
+
+c, a, t = "cat"
+print(c, a, t)
+
+y = 2
+x = y*3
+
+x = 1
+x = x+1
+x+=1
+print(x)
+
+very_important_object = 1
+
+# Very_Important_Object = 10
+
+
+print(2**3*2)
+print(2**(3*2))
+
+print(10%3)
+
+print(15/3)
+print(15//3)
+print(16//3)
+print(17//3)
+print(18//3)
+
+print(2 == 2.0)
+print(2 == 2.00000000001)
+print(2 == 2.0000000000000001)
+
+print(2 > 3)
+print(x <= 10)
+
+if True:
+    print("it's true")
+
+# int x = 10 - it's C
+x = 10
+print(type(x))
+
+x = 10.1
+print(type(x))
---
+++ Lessons/lesson 15.10/pif.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the a value: "))
+c = int(input("Enter the c value: "))
+
+b = int((c*c-a*a)**0.5)
+print("your answer is", b)
