4fe96fe1356c8f074a9ae4e2b5b30aefc62eac2a
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2023-10-21 09:43:00 +0300

    clean up

--- Tasks/Zhdanovich.Task1.task1.py
+++
@@ -1,8 +0,0 @@
-try:
-    # enter the  value in degrees Celsius
-    a = float(input("enter the temperature in degrees Celsius"))
-    # determine the temperature in degrees Fahrenheit (formule)
-    b = float((5 / 9) * a + 32)
-    print("temperature in degrees Farengeyt", "=", round(b, 1))
-except (ValueError):
-    print("You can only enter a numeric value")
--- Tasks/Zhdanovich.Task1.task3.py
+++
@@ -1,8 +0,0 @@
-try:
-    # Enter a value of the speed in kilometers per hour
-    v1=float(input("Enter a value of the speed in kilometers per hour"))
-    #determine the speed in meters per second (formule)
-    v2=float(v1*(1000/3600))
-    print("The speed in meters per second"," =", round(v2,2))
-except (ValueError):
-    print("You can only enter a numeric value")
