ab337082df306b1ce30ade134c6c45eecac31f46
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-10-25 18:51:02 +0300

    def

--- Tasks/Linnik/Task2/task.py
+++ Tasks/Linnik/Task2/task.py
@@ -1,5 +1,19 @@
+def to_next_hour(hhmm, next='we_need'):
+    ''' Данная функция создана для уменьшения объёма словаря и перехода к следующему через уже знакомы числа. '''
+    if next == 'we_need':
+        if hhmm[0]+1 > 12:
+            hhmm[0] = hhmm[0]+1 - 12
+        else:
+            hhmm[0] = hhmm[0] + 1
+        return hhmm[0]
+    else:
+        if hh_mm[0] > 12:
+            hh_mm[0] = hh_mm[0] - 12
+        return hhmm[0]
+
+
 numbers = {
-    0: [],
+    0: [''],
     1: ['', "первого"],
     2: ["два", "второго"],
     3: ["три", "третьего"],
@@ -34,8 +48,7 @@ if len(user_time) > 5 or hh_mm[0] > 24 or hh_mm[1] > 59:
 
 # :} min == 0: такое-то значение часа ровно (15:00 - три часа ровно)
 elif hh_mm[1] == 0:
-    if hh_mm[0] > 12:
-        hh_mm[0] = hh_mm[0] - 12
+    hh_mm[0] = to_next_hour(hh_mm, 'no')
     if hh_mm[0] == 0 and hh_mm[1] == 0:
         print("Час ночи у вас")
     elif hh_mm[0] == 1:
@@ -47,11 +60,7 @@ elif hh_mm[1] == 0:
 
 # :} min < 30: столько-то минут следующего часа (19:12 - двенадцать минут восьмого)
 elif hh_mm[1] < 30:
-    if hh_mm[0]+1 > 12:
-        hh_mm[0] = hh_mm[0]+1 - 12
-    elif hh_mm[0] == 0:
-        hh_mm[0] = hh_mm[0] + 1
-
+    hh_mm[0] = to_next_hour(hh_mm)
     if hh_mm[1] == 1:
         print(f'{user_time} - одна минута {numbers[hh_mm[0]][1]}')
     elif hh_mm[1] == 2:
@@ -69,11 +78,16 @@ elif hh_mm[1] < 30:
 
 # :} min == 30: половина такого-то (15:30 - половина четвёртого)
 elif hh_mm[1] == 30:
-    print(f'{user_time} - половина {numbers[hh_mm[0]+1][1] if hh_mm[0]+1 <= 12 else numbers[hh_mm[0]+1-12][1]}')
+    hh_mm[0] = to_next_hour(hh_mm)
+    print(f'{user_time} - половина {numbers[hh_mm[0]][1]}')
 
 # min > 30 and min < 45 столько-то минут следующего часа (12:38 - тридцать восемь минут первого)
 elif 30 < hh_mm[1] < 45:
-    print(f'{user_time} - ')
+    hh_mm[0] = to_next_hour(hh_mm)
+    if hh_mm[1] < 40:
+        print(f'{user_time} - тридцать {numbers[int(str(hh_mm[1])[-1])][0]} минут {numbers[hh_mm[0]][1]}')
+    else:
+        print(f'{user_time} - сорок {numbers[int(str(hh_mm[1])[-1])][0]} минут {numbers[hh_mm[0]][1]}')
 
 # min >= 45 без min такого-то (08:54 - без шести минут девять)
 elif hh_mm[1] >= 45:
