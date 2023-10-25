0c3a1309def5fa120e3c7a7a9294b3f905adefc0
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-10-25 21:19:24 +0300

    part4

--- Tasks/Linnik/Task2/task.py
+++ Tasks/Linnik/Task2/task.py
@@ -44,9 +44,10 @@ numbers = {
 print("Введите время в виде чч:мм")
 user_time = f"{input('Который час? ')}:{input('Сколько минут? ')}"
 
-hh_mm = user_time.split(':')
-hh_mm[0] = int(hh_mm[0])
-hh_mm[1] = int(hh_mm[1])
+hh_mm = []
+separated_time = user_time.split(':')
+hh_mm.append(int(separated_time[0]))
+hh_mm.append(int(separated_time[1]))
 
 if len(user_time) > 5 or hh_mm[0] > 24 or hh_mm[1] > 59:
     print("Напишите правильно время")
@@ -90,10 +91,20 @@ elif hh_mm[1] == 30:
 # min > 30 and min < 45 столько-то минут следующего часа (12:38 - тридцать восемь минут первого)
 elif 30 < hh_mm[1] < 45:
     hh_mm[0] = to_next_hour(hh_mm)
-    if hh_mm[1] < 40:
+    if hh_mm[1] == 31:
+        print(f'{user_time} - тридцать одна минута {numbers[hh_mm[0]][1]}')
+    elif hh_mm[1] == 32:
+        print(f'{user_time} - тридцать две минуты {numbers[hh_mm[0]][1]}')
+    elif 32 < hh_mm[1] < 35:
+        print(f'{user_time} - тридцать {numbers[int(str(hh_mm[1])[-1])][0]} минуты {numbers[hh_mm[0]][1]}')
+    elif 35 <= hh_mm[1] <= 40:
         print(f'{user_time} - тридцать {numbers[int(str(hh_mm[1])[-1])][0]} минут {numbers[hh_mm[0]][1]}')
+    elif hh_mm[1] == 41:
+        print(f'{user_time} - сорок одна минута {numbers[hh_mm[0]][1]}')
+    elif hh_mm[1] == 42:
+        print(f'{user_time} - сорок две минуты {numbers[hh_mm[0]][1]}')
     else:
-        print(f'{user_time} - сорок {numbers[int(str(hh_mm[1])[-1])][0]} минут {numbers[hh_mm[0]][1]}')
+        print(f'{user_time} - сорок {numbers[int(str(hh_mm[1])[-1])][0]} минуты {numbers[hh_mm[0]][1]}')
 
 # min >= 45 без min такого-то (08:54 - без шести минут девять)
 elif hh_mm[1] >= 45:
