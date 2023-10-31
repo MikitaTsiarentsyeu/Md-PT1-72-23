d7c43229912d6b5908e150d95f4b762dbb970770
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-10-31 17:06:38 +0300

    First

--- Tasks/Linnik/Task3/Task3_9.py
+++ Tasks/Linnik/Task3/Task3_9.py
@@ -1,1 +1,1 @@
-# 9. Write a program that takes a string as input and returns the string reversed.
+# 9. Write a program that takes a string as input and returns the string reversed. (without slice)
