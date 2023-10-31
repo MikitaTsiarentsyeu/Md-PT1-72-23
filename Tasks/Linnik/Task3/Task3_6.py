b09817b1af51dd01f14fdc69bc961928477c852d
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-10-31 18:05:57 +0300

    Sixth

--- Tasks/Linnik/Task3/Task3_6.py
+++ Tasks/Linnik/Task3/Task3_6.py
@@ -1,1 +1,8 @@
 # 6. Write a program that takes a string as input and returns the string with all vowels removed.
+string = input()
+vowels = ('a', 'e', 'o', 'i', 'u', 'A', 'E', 'O', 'I', 'U')
+for i in range(len(string)):
+    if string[i] not in vowels:
+        print(string[i], end='')
+
+
