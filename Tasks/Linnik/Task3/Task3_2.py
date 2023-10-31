1f1b63c025687d40afcfe8f8b8eb178027a776b1
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-10-31 17:18:53 +0300

    Second

--- Tasks/Linnik/Task3/Task3_2.py
+++ Tasks/Linnik/Task3/Task3_2.py
@@ -1,1 +1,9 @@
-# 2. Write a program that takes a list of numbers as input and returns the sum of all even numbers in the list.
+# 2. Write a program that takes a list of numbers as input a
+# returns the sum of all even numbers in the list.
+string_of_numbers = input('Enter your numbers in this string: ')
+list_of_numbers = [int(i) for i in string_of_numbers.split()]
+sum_even = 0
+for i in list_of_numbers:
+    if i % 2 == 0:
+        sum_even += i
+print(sum_even)
