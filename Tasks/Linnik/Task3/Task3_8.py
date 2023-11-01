3030f4cebb1385b0daba73df9434fd8a19faa83c
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-01 16:41:19 +0300

    Eighth

--- Tasks/Linnik/Task3/Task3_8.py
+++ Tasks/Linnik/Task3/Task3_8.py
@@ -1,2 +1,7 @@
 # 8. Write a program that takes a list of numbers as input
 # returns the average of all numbers in the list.
+numbers = [1, 2, 3, 45, 56, 76, 100]
+average = 0
+for i in numbers:
+    average += i
+print(average/len(numbers))
