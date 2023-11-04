46b655eec85d1ff0aa109e7a5ea076feed20cc72
DzmitryZhdanovich <147974528+DzmitryZhdanovich@users.noreply.github.com>
2023-11-04 21:58:13 +0300

    Add files via upload

---
+++ task3_7.py
@@ -0,0 +1,4 @@
+# String for input
+# People start learning a foreign Language, because they want to have a better Job.
+user_input = input('Input anything: ')
+print (user_input.swapcase())
---
+++ task3_8.py
@@ -0,0 +1,6 @@
+
+a = list(map(int, input('Enter your numbers through spaces: ').split()))
+#print(a)
+#print(sum(a))
+average_a = sum(a)/len(a)
+print(average_a)
