4781dab074a72907c5ce3c1cb3da2c399bc795c2
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-10 23:58:39 +0300

    Finish?

--- Tasks/Linnik/Task4/task.py
+++ Tasks/Linnik/Task4/task.py
@@ -1,18 +1,3 @@
-# 3. Format the text taking into account the maximum number of characters,
-# but if a word does not fit entirely on a line,
-# it should be moved to the next one, and the spacing between words should be evenly increased
-
-# if '\n' in chunk:
-    #     return n, line
-    # Если /n в середине, то следующей строке будет больше символов
-
-# def check_line(n, line):
-#     # next_symbol = t.read(1)
-#     # if next_symbol == ' ' or next_symbol == '\n':
-#     #     return n, line + '\n'
-#     return n, line + '\n'
-
-
 number = 0
 while number <= 35:
     number = int(input('Enter your number of characters per line and this number must be greater than 35: '))
@@ -23,6 +8,7 @@ with open('text.txt', 'r', encoding='utf8') as t:
         while next_symbol:
             chunk = t.read(number)
             next_symbol = t.read(1)
+            t.seek(t.tell() - 1)
             if chunk == '':
                 break
 
@@ -31,23 +17,45 @@ with open('text.txt', 'r', encoding='utf8') as t:
             elif '\n' in chunk[1:number-2]:
                 ind = chunk.index('\n')
                 chunk = chunk[:ind]
+                rewrited_t.write(chunk + '\n')
+                # Return the cursor back:
                 lost_symbols = number - len(chunk)
-                #//
-
+                t.seek(t.tell() - lost_symbols + 1)
             elif next_symbol in (' ', '\n'):
                 rewrited_t.write(chunk+'\n')
+                t.seek(t.tell() + 1)
             elif chunk[number-1] in (' ', '\n'):
-                chunk = chunk.replace(' ', '  ', 1)  # --> 'text  text '
                 if chunk[number-1] == ' ':
                     chunk = chunk[:number-1]  # --> 'text  text'
+                    chunk = chunk.replace(' ', '  ', 1)  # --> 'text  text '
                     rewrited_t.write(chunk + '\n')
                 elif chunk[number-1] == '\n':
+                    chunk = chunk.replace(' ', '  ', 1)  # --> 'text  text '
                     rewrited_t.write(chunk)
             elif chunk[0] in (' ', '\n'):
                 chunk = chunk[1:]
                 chunk = chunk.replace(' ', '  ', 1)
                 rewrited_t.write(chunk + '\n')
-            # else:
-
-            t.seek(t.tell()-1)
+            else:
+                ind = chunk.rfind(' ')
+                chunk = chunk[:ind]
+                lost_symbols = number - len(chunk)
+                t.seek(t.tell() - lost_symbols + 1)  # +1 <--> not to start from a whitespace
+                new_chunk = list(chunk)
+                while lost_symbols != 0:
+                    counter = 0
+                    iter_chunk = tuple(new_chunk)
+                    for i in range(len(iter_chunk)):
+                        if lost_symbols == 0:
+                            break
+                        if iter_chunk[i] == ' ':
+                            new_chunk.insert(counter, ' ')
+                            counter += 1
+                            lost_symbols -= 1
+                        counter += 1
+                new_chunk_str = ''
+                for i in new_chunk:
+                    new_chunk_str += i
+                rewrited_t.write(new_chunk_str + '\n')
+                del new_chunk, iter_chunk
 print('We did it!')
