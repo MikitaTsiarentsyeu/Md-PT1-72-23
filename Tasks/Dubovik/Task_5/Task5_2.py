a712dc9ad2c4b2ed7d77b687418626440e8ee51f
dubovix <a.a.dubovik@gmail.com>
2023-11-16 19:22:06 +0300

    Task5_1 and Task5_2 done

---
+++ Tasks/Dubovik/Task_5/Task5_2.py
@@ -0,0 +1,10 @@
+'''2. Write a function that takes a
+list of strings as an argument and returns a new list of
+strings that are all reversed.'''
+str = ["My", "name", "is", "Alex"]
+def rev_str(str):
+    str2 = str[::-1]
+    return str2
+
+print(rev_str(str))
+
