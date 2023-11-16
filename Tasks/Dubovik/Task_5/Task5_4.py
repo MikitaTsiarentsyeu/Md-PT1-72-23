002f53252d9ea88c14d2dbaf39f3bde97ba9850e
dubovix <a.a.dubovik@gmail.com>
2023-11-16 23:47:00 +0300

    Task5_3 done

---
+++ Tasks/Dubovik/Task_5/Task5_4.py
@@ -0,0 +1,4 @@
+'''4. Write a function that takes a string as an argument and returns two numbers,
+first for count of lower case letters, second for count of the upper case letters in the initial string.
+'''
+
