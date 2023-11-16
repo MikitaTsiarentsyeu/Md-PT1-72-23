3cdd62e535db0b9807c4cbd9dbaa5410d02710cc
dubovix <a.a.dubovik@gmail.com>
2023-11-17 01:01:53 +0300

    Task5_4 done

--- Tasks/Dubovik/Task_5/Task5_4.py
+++ Tasks/Dubovik/Task_5/Task5_4.py
@@ -1,4 +1,17 @@
 '''4. Write a function that takes a string as an argument and returns two numbers,
 first for count of lower case letters, second for count of the upper case letters in the initial string.
 '''
+str = "AAAooooouuuIIII"
+str2 = "4. Write a function that takes a string as an argument and returns two numbers,first for count of lower case letters, second for count of the upper case letters in the initial string."
+def upper_lower(str):
+    upper = 0
+    lower = 0
+    for i in str:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+    return (f'Uppercase letters:{upper},Lowercase letters:" {lower}')
+print(upper_lower(str))
+print(upper_lower(str2))
 
