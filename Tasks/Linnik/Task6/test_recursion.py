e21aec6006a553aa3d6f40849830fc534ff4a107
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-17 22:00:41 +0300

    Start

---
+++ Tasks/Linnik/Task6/test_recursion.py
@@ -0,0 +1,20 @@
+import unittest
+import recursion
+
+
+class TestRecursion(unittest.TestCase):
+
+    def test_reverse_str(self):
+        pass
+
+    def test_palindrome(self):
+        pass
+
+    def test_num_letter(self):
+        pass
+
+    def test_pow_num(self):
+        pass
+
+    def test_fibonacci(self):
+        pass
