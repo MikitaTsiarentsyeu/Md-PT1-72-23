a454bdaf973d06035103eba18b33c4bc6a0c5219
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-17 20:49:51 +0300

    Create Task5_4

---
+++ Tasks/Linnik/Task5/Task5_4.py
@@ -0,0 +1,35 @@
+def count_lower_and_upper(string: str) -> tuple:
+    """
+    The function returns a number of lowercase letters and a number of uppercase letters.
+    :string: str.
+    :return: tuple(num_lowercase_letters, num_uppercase_letters)
+    """
+    no_letters = r"1234567890,./?<|>\ `~!@#№$%:;^&*()_-+={}[]'" + '"'
+    lowercase_letters = 0
+    uppercase_letters = 0
+    low_str = string.lower()
+    up_str = string.upper()
+
+    for i, e in enumerate(string):
+        if e in no_letters:
+            continue
+        if e == low_str[i]:
+            lowercase_letters += 1
+
+    for i, e in enumerate(string):
+        if e in no_letters:
+            continue
+        if e == up_str[i]:
+            uppercase_letters += 1
+
+    return lowercase_letters, uppercase_letters
+
+
+print(count_lower_and_upper(''))
+print(count_lower_and_upper(' '))
+print(count_lower_and_upper(','))
+print('little', count_lower_and_upper('маленькие буквы'), len('маленькие буквы'))
+print('BIG-1', count_lower_and_upper('БОЛЬШИЕ БУКВЫ'), len('БОЛЬШИЕ БУКВЫ'))
+print('BIG-2', count_lower_and_upper('БОЛЬШИЕ_БУКВЫ_'), len('БОЛЬШИЕ_БУКВЫ_'))
+print(count_lower_and_upper('Такой текст'), len('Такой текст'))
+print(count_lower_and_upper('или ТаКоЙ бЫвАеТ'), len('или ТаКоЙ бЫвАеТ'))
