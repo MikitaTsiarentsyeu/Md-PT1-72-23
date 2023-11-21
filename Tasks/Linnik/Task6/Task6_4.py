a9e60d980c000056d54f7da1f407f6b9378b803f
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-21 20:34:08 +0300

    Update

--- Tasks/Linnik/Task6/Task6_4.py
+++ Tasks/Linnik/Task6/Task6_4.py
@@ -1,10 +1,20 @@
-# 4. Write a recursive function to calculate the power of a given number.
-def pow_num(num, power):
-    pass
+def pow_num(num: int, power: int, mult=1) -> int:
+    """
+    The recursive function to calculate the power of a given number.
+    num: int,
+    power: int,
+    mult: int.
+    Return: int.
+    """
+    if power == 0:
+        return mult
+    else:
+        mult *= num
+        return pow_num(num, power-1, mult)
 
-# self.assertEqual(recursion.pow_num(4, 4), 256)
-#         self.assertEqual(recursion.pow_num(3, 3), 27)
-#         self.assertEqual(recursion.pow_num(2, 2), 4)
-#         self.assertEqual(recursion.pow_num(1, 1), 1)
-#         self.assertEqual(recursion.pow_num(0, 0), 1)
-#         self.assertEqual(recursion.pow_num(2, -1), 0.5)
+
+print(pow_num(4, 4), 256)
+print(pow_num(3, 3), 27)
+print(pow_num(2, 2), 4)
+print(pow_num(1, 1), 1)
+print(pow_num(0, 0), 1)
