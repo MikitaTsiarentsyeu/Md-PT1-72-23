906d1dd0ee70b89b0bbb17931b458b6ae449dc7c
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-21 20:13:09 +0300

    Palindrome

--- Tasks/Linnik/Task6/Task6_2.py
+++ Tasks/Linnik/Task6/Task6_2.py
@@ -1,8 +1,24 @@
-# 2. Write a recursive function to check whether a given string is a palindrome.
-def palindrome(string):
-    pass
+def palindrome(string: str, st=0, end=0) -> bool:
+    """
+    The recursive function checks a given string is a palindrome.
+    string: str.
+    st: int.
+    end: int.
+    return: bool.
+    """
+    end = len(string) - 1
+    if len(string) in (1, 0):
+        return True
+    if st + 1 > len(string) // 2:
+        return True
+    if string[st] == string[end]:
+        return palindrome(string, st+1, end-1)
+    else:
+        return False
 
-# self.assertTrue(recursion.palindrome('wow'))
-#         self.assertTrue(recursion.palindrome('k'))
-#         self.assertTrue(recursion.palindrome(''))
-#         self.assertFalse(recursion.palindrome('key'))
+
+print(palindrome(''))
+print(palindrome('k'))
+print(palindrome('wow'))
+print(palindrome('key'))
+print(palindrome('wdfhjfdw'))
