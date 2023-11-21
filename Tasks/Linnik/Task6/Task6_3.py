595735a12a394e5247927b38d1e417ad686a736c
ZarinaLinnik <zarinaelan2005@gmail.com>
2023-11-21 20:21:47 +0300

    Symbol and str

--- Tasks/Linnik/Task6/Task6_3.py
+++ Tasks/Linnik/Task6/Task6_3.py
@@ -1,8 +1,21 @@
-# 3. Write a recursive function to count the number of occurrences of a given character in a string.
-def num_letter(symbol, string):
-    pass
+def num_letter(symbol: str, string: str, index=0, counter=0) -> int:
+    """
+    This recursive function counts the number of occurrences of a given character in a string.
+    symbol: str,
+    string: str,
+    index: int,
+    counter: int.
+    Return: int.
+    """
+    if index == len(string):
+        return counter
+    else:
+        if string[index] == symbol:
+            counter += 1
+        return num_letter(symbol, string, index+1, counter)
 
-# self.assertEqual(recursion.num_letter('a', 'Alana'), 2)
-#         self.assertEqual(recursion.num_letter('b', 'bobb'), 3)
-#         self.assertEqual(recursion.num_letter('O', 'Olow'), 1)
-#         self.assertEqual(recursion.num_letter('4', '2344544'), 4)
+
+print(num_letter('a', 'Alana'))
+print(num_letter('b', 'bobb'))
+print(num_letter('O', 'Olow'))
+print(num_letter('4', '2344544'))
