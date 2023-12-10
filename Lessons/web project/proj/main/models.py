9f92f02d7aa83499b91d2e620c5dbd5150f83616
MikitaTsiarentsyeu <terentievnikita@gmail.com>
2023-12-10 10:47:47 +0200

    add lesson 10/12

--- Lessons/web project/proj/main/models.py
+++ Lessons/web project/proj/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(primary_key=True)
+
+class Post(models.Model):
+
+    POST_TYPES = [('c', "copyright"), ('p', "public license")]
+
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    post_type = models.CharField(max_length=1, choices=POST_TYPES)
+    issued = models.DateTimeField()
+    image = models.ImageField(upload_to='uploads')
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
